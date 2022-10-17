8f1db8fd7610fddd528f6d9fd3dcf2dd3b773d02
tonysnickers <antoine.mendy@hotmail.fr>
2022-10-17 11:48:04 +0200

    list of reataurant added

--- src/components/FetchRestaurant.js
+++ src/components/FetchRestaurant.js
@@ -1,17 +1,27 @@
 import axios from 'axios'
 import React, { useEffect } from 'react'
+import RestaurantsLists from './RestaurantsLists'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import CardMedia from '@mui/material/CardMedia'
+import Typography from '@mui/material/Typography'
+import { CardActionArea } from '@mui/material'
 
-const FetchRestaurant = ({setRestaurants, restaurants}) => {
-
-
+const FetchRestaurant = ({ setRestaurants, restaurants }) => {
   useEffect(() => {
-    axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?s=beef`).then((res) => setRestaurants(res.data.meals))
+    axios
+      .get(`https://www.themealdb.com/api/json/v1/1/search.php?s=beef`)
+      .then((res) => setRestaurants(res.data.meals))
   })
   return (
     <div>
-      <ul>
-        {restaurna}
-      </ul>
+        {restaurants.map((restaurant) => (
+      <Card sx={{ maxWidth: 345 }}>
+          <CardActionArea>
+            <RestaurantsLists restaurant={restaurant} />
+          </CardActionArea>
+      </Card>
+            ))}
     </div>
   )
 }
